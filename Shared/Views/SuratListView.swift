a39a5bb3825748cf2fa4c067e3181ce39af7c7e2
Muhammad Irfan <irfan.pule2@gmail.com>
2022-04-26 15:21:59 +0700

    search surah

--- Shared/Views/SuratListView.swift
+++ Shared/Views/SuratListView.swift
@@ -10,10 +10,11 @@ import SwiftUI
 struct SuratListView: View {
     @State var surats: [SuratModel] = []
     @State private var isLoading = false
+    @State var searchQuery = ""
     
     var body: some View {
         ZStack {
-            List(surats, id: \.nomor) { surat in
+            List(searchResults, id: \.nomor) { surat in
                 NavigationLink(
                     destination: SuratDetailView(surat: surat),
                     label: {
@@ -29,7 +30,9 @@ struct SuratListView: View {
             if isLoading {
                 LoadingView()
             }
+            
         }
+        .searchable(text: $searchQuery, prompt: "Cari dengan nama surat")
     }
     
     func getData() {
@@ -39,10 +42,19 @@ struct SuratListView: View {
             isLoading = false
         }
     }
+    
+    var searchResults: [SuratModel] {
+            if searchQuery.isEmpty {
+                return self.surats
+            } else {
+                return self.surats.filter { $0.nama_latin.contains(searchQuery) }
+            }
+        }
 }
 
 struct SuratListView_Previews: PreviewProvider {
     static var previews: some View {
         SuratListView(surats: SuratModel.dummy())
+            .previewDevice("iPhone Xs")
     }
 }
